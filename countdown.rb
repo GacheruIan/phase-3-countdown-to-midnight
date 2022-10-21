2bb43043f53b4fbaf38918912a7b43ccec4f53e3
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-10-21 13:56:45 +0300

    Done assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
-
-def countdown
+require 'pry'
+def countdown(x)
+    # x = 10
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+         x-=1
+    end 
+   'HAPPY NEW YEAR!'
 end
+countdown(10)
+
+def countdown_with_sleep(x)
+    return sleep 5
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+         x-=1
+    end 
+   'HAPPY NEW YEAR!'
+   
+end
+countdown_with_sleep(10)
